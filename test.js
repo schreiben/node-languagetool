99a63e76c85d7bdaa215e133d3bfb07d77bef51d
Tilman Kamp <tilmankamp@posteo.de>
2016-11-26 19:18:22 +0100

    Added test output

--- test.js
+++ test.js
@@ -22,6 +22,7 @@
  */
 
 const process = require('process');
+console.log('Testing...');
 require('.').smoketest().then(
   () => { console.log('Success'); process.exit(0); },
   err => { console.error(err); process.exit(1); }
